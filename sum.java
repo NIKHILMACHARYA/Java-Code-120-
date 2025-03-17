e24022c11ec7a120abe0988bf513e67b5173771c
Nikhil M Acharya <154912600+NIKHILMACHARYA@users.noreply.github.com>
2025-03-17 22:35:17 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -2,7 +2,7 @@ public class sum{
     public static void main(String [] args){
        int j=0;
        int sum=0;
-       for9int i=1;i<=5;i++){
+       for(int i=1;i<=5;i++){
         sum=sum+ i;
 
        }
