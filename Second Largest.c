de21b0cced259a538cfb6ea1d98cd15c9b74a260
Nikhil M Acharya <154912600+NIKHILMACHARYA@users.noreply.github.com>
2025-03-26 22:56:37 +0530

    Update Second Largest.c

--- Second Largest.c
+++ Second Largest.c
@@ -1,32 +1,50 @@
-#include<stdio.h>
-int second_largest_element(int arr[], int size){
-    int first_largst,second_largest,i;
-    if(size<2){
-        printf("Error: Array has less than 2 elements!");
-        return 0;
+#include <stdio.h>
+#include <limits.h>
+
+int second_largest_element(int arr[], int size) {
+    int first_largest = INT_MIN, second_largest = INT_MIN, i;
+
+    if (size < 2) {
+        printf("Error: Array has less than 2 elements!\n");
+        return -1; // Return -1 to indicate error
     }
-    for(i=0;i<size;i++){
-        if(first_largst<arr[i]){
-            second_largest=first_largst;
-            first_largst=arr[i];
+
+    for (i = 0; i < size; i++) {
+        if (arr[i] > first_largest) {
+            second_largest = first_largest; // Update second largest
+            first_largest = arr[i]; // Update first largest
+        } else if (arr[i] > second_largest && arr[i] != first_largest) {
+            second_largest = arr[i]; // Update second largest
         }
-        else if(
-            arr[i]!= first_largst && second_largest < arr[i] &&
-            second_largest<first_largst)
-                second_largest=arr[i];
-     }
+    }
+
+    if (second_largest == INT_MIN) {
+        printf("There is no second largest element (all elements may be equal).\n");
+        return -1; // Return -1 to indicate error
+    }
+
     return second_largest;
 }
 
-void main(){
-     int size, index;
-     printf("Enter the size of array:");
-     scanf("%d",&size);
-     int arr[size];
-     printf("Enter the array elements:\n");
-     for(index=0; index<size; index++){
-        scanf("%d",&arr[index]);
-     }
-     printf("Second largest Element is : %d", second_largest_element(arr,size));
+int main() {
+    int size, index;
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+    if (size < 2) {
+        printf("Error: Array must have at least 2 elements.\n");
+        return 1; // Exit with an error code
+    }
 
+    int arr[size];
+    printf("Enter the array elements:\n");
+    for (index = 0; index < size; index++) {
+        scanf("%d", &arr[index]);
+    }
+
+    int result = second_largest_element(arr, size);
+    if (result != -1) {
+        printf("Second largest element is: %d\n", result);
+    }
+    
+    return 0; // Return 0 to indicate successful completion
 }
