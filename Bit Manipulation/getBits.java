09a891f2149ca399fc8e86d451f72355471cd945
Nikhil M Acharya <154912600+NIKHILMACHARYA@users.noreply.github.com>
2025-06-17 21:41:06 +0530

    Update getBits.java

--- Bit Manipulation/getBits.java
+++ Bit Manipulation/getBits.java
@@ -2,15 +2,14 @@ import java.util.*;
 
 public class getBits {
     public static void main(String[] args) {
-      int n = 5;
-      int pos = 2;
-      int bitMask = 1 << pos;
-      
-      if([bitMask & n] == 0){
-        System.out.println("Bit was zero");
-      }
-      else{
-        System.out.println("Bit was one");
-      }
-  }
+        int n = 5;
+        int pos = 2;
+        int bitMask = 1 << pos;
+
+        if ((bitMask & n) == 0) {
+            System.out.println("Bit was zero");
+        } else {
+            System.out.println("Bit was one");
+        }
+    }
 }
