27da64bfca86753c70eb1a98482fd75971f5a0f2
Nikhil M Acharya <154912600+NIKHILMACHARYA@users.noreply.github.com>
2025-06-22 21:41:31 +0530

    Add files via upload

---
+++ Sorting Methods/SelectionSort.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {7, 8, 3, 1, 2};
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+
+            // Find index of the smallest element in the unsorted part
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // Swap with the first unsorted element
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+
+        // Print sorted array
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
