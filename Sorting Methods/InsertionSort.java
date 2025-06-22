1a2bccc78c43b18029bf3ebaf905284c73a9f03c
Nikhil M Acharya <154912600+NIKHILMACHARYA@users.noreply.github.com>
2025-06-22 21:41:54 +0530

    Add files via upload

---
+++ Sorting Methods/InsertionSort.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr = {7, 8, 3, 1, 2};
+
+        for (int i = 1; i < arr.length; i++) {
+            int current = arr[i];
+            int j = i - 1;
+
+            // Shift elements that are greater than current
+            while (j >= 0 && arr[j] > current) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            // Insert current at correct position
+            arr[j + 1] = current;
+        }
+
+        // Print sorted array
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
