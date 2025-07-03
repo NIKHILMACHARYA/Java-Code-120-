087f0ed7a749ca3319a1b945404ba635eb41c388
Nikhil M Acharya <154912600+NIKHILMACHARYA@users.noreply.github.com>
2025-07-03 19:12:16 +0530

    Add files via upload

---
+++ ReverseString.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class ReverseString {
+    public static void main(String[] args) {
+      String original= "automation";
+      String reversed= new StringBuilder(original).reverse().toString();
+      System.out.println("Reversed: "+reversed);
+  }
+}
+
+
+Output:
+
+Reversed: noitamotua
