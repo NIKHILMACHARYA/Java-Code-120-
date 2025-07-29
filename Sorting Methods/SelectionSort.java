89bc5878c1af1013ba725805242d4295e0b74ef4
Nikhil M Acharya <154912600+NIKHILMACHARYA@users.noreply.github.com>
2025-07-29 07:49:12 +0530

    Update SelectionSort.java

--- Sorting Methods/SelectionSort.java
+++ Sorting Methods/SelectionSort.java
@@ -5,21 +5,18 @@ public class SelectionSort {
 
         for (int i = 0; i < arr.length - 1; i++) {
             int minIndex = i;
-
             // Find index of the smallest element in the unsorted part
             for (int j = i + 1; j < arr.length; j++) {
                 if (arr[j] < arr[minIndex]) {
                     minIndex = j;
                 }
             }
-
             // Swap with the first unsorted element
             int temp = arr[minIndex];
             arr[minIndex] = arr[i];
             arr[i] = temp;
         }
 
-        // Print sorted array
         for (int num : arr) {
             System.out.print(num + " ");
         }
