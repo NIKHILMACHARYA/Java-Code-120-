39f2778accfaf1c170de44e0e5806c3a1d250103
Nikhil M Acharya <154912600+NIKHILMACHARYA@users.noreply.github.com>
2025-08-03 17:50:57 +0530

    Update MergeSort.java

--- Sorting Methods/MergeSort.java
+++ Sorting Methods/MergeSort.java
@@ -1,22 +1,17 @@
 import java.util.*;
 public class MergeSort {
-
-    // Merge two sorted halves
     public static void merge(int[] arr, int left, int mid, int right) {
         // Sizes of two subarrays
         int n1 = mid - left + 1;
         int n2 = right - mid;
-
         // Temp arrays
         int[] L = new int[n1];
         int[] R = new int[n2];
-
         // Copy data to temp arrays
         for (int i = 0; i < n1; i++)
             L[i] = arr[left + i];
         for (int j = 0; j < n2; j++)
             R[j] = arr[mid + 1 + j];
-
         // Merge temp arrays
         int i = 0, j = 0, k = left;
         while (i < n1 && j < n2) {
@@ -29,7 +24,6 @@ public class MergeSort {
             }
             k++;
         }
-
         // Copy remaining elements
         while (i < n1) {
             arr[k++] = L[i++];
@@ -39,17 +33,14 @@ public class MergeSort {
             arr[k++] = R[j++];
         }
     }
-
     // Main sort function
     public static void mergeSort(int[] arr, int left, int right) {
         if (left < right) {
             int mid = left + (right - left) / 2;
-
             // Recursively sort two halves
             mergeSort(arr, left, mid);
             mergeSort(arr, mid + 1, right);
-
-            // Merge them
+          // Merge them
             merge(arr, left, mid, right);
         }
     }
@@ -58,10 +49,10 @@ public class MergeSort {
         int[] arr = {7, 8, 3, 1, 2};
 
         mergeSort(arr, 0, arr.length - 1);
-
         // Print sorted array
         for (int num : arr) {
             System.out.print(num + " ");
         }
     }
 }
+
