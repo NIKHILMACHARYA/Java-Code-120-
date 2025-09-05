d89b59d4a21a02394fff7c3c9bd8dd0c35e024c9
Nikhil M Acharya <154912600+NIKHILMACHARYA@users.noreply.github.com>
2025-09-05 22:37:42 +0530

    Update FacorialNmbr.java

--- FacorialNmbr.java
+++ FacorialNmbr.java
@@ -1,13 +1,13 @@
 public class FacorialNmbr{
   public static void main(String [] args){
     int num = 5;
-    
+    // Recursive Method
     int factorial = factorialRecursive(num);
     System.out.println("Factorial of "+num+" : "+factorial);
   }
 
   public static int factorialRecursive(int n){
-    if(n==0 | n==1) return 1;
+    if(n==0 || n==1) return 1;
     return n * factorialRecursive(n-1);
   }
 }
