dfce303701251718ff19b6d0f564cbc506ae1886
Nikhil M Acharya <154912600+NIKHILMACHARYA@users.noreply.github.com>
2025-10-13 23:37:37 +0530

    Add files via upload

---
+++ SumofDigits.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class SumofDigits {
+    public static void main(String[] args) {
+      int Numbers= 12345;
+      int sum=0;
+      while(Numbers>0){
+        sum += Numbers % 10;
+        Numbers /=10;
+      }
+      System.out.println("Sum of digits: "+sum);
+  }
+}
+
+
+Output:
+
+Sum of digits: 15
