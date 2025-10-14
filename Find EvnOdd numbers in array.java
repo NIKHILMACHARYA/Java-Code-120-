646ea363f9e93be5f10229a7d12d65134c6defac
Nikhil M Acharya <154912600+NIKHILMACHARYA@users.noreply.github.com>
2025-10-14 23:59:43 +0530

    Create Find EvnOdd numbers in array.java

---
+++ Find EvnOdd numbers in array.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+public class EvenOdd{
+  public static void main(String [] args){
+    int[] numbers= {1,2,3,4,5,6,7,8,9};
+    ArrayList evan=new ArrayList<>();
+    ArrayList odd=new ArrayList<>();
+
+  
