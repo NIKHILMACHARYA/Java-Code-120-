54d649eed9362a35cc9a88ee1a966f1efb2ea0ed
Nikhil M Acharya <154912600+NIKHILMACHARYA@users.noreply.github.com>
2025-10-15 00:28:52 +0530

    Update TowerHonai.java

--- Recursion Programs/TowerHonai.java
+++ Recursion Programs/TowerHonai.java
@@ -1,18 +1,33 @@
 import java.util.*;
 
-public class TowerHonai{
-    public static void TowerofHonai(int n, string str, string helper, string dest ){
-        if(n==1){
-            System.out.println("Transfer disk "+n+" from "+src+" to "+dest);
+public class TowerHonai {
+    public static void TowerofHonai(int n, String src, String helper, String dest) {
+        if (n == 1) {
+            System.out.println("Transfer disk " + n + " from " + src + " to " + dest);
             return;
         }
-        TowerofHonai(n-1, src, dest ,helper);
-        System.ou.println("Transfer disk "+n+" from "+src+" to "+dest);
-        TowerofHonai(n-1, helper, src, dest);
+
+        TowerofHonai(n - 1, src, dest, helper);
+        System.out.println("Transfer disk " + n + " from " + src + " to " + dest);
+        TowerofHonai(n - 1, helper, src, dest);
     }
 
-    public static void main(string[] args){
-        int n=3;
-        TowerofHonai(n ,"S", "H", "D")
+    public static void main(String[] args) {
+        int n = 3;
+        TowerofHonai(n, "S", "H", "D");
     }
-}    
+}
+
+
+
+// Output:
+
+// Transfer disk 1 from S to D
+// Transfer disk 2 from S to H
+// Transfer disk 1 from D to H
+// Transfer disk 3 from S to D
+// Transfer disk 1 from H to S
+// Transfer disk 2 from H to D
+// Transfer disk 1 from S to D
+
+    
