9145c3189a6fb045bcbb6a6dde15c1227af04896
Nikhil M Acharya <154912600+NIKHILMACHARYA@users.noreply.github.com>
2025-10-15 00:06:06 +0530

    Update Find EvnOdd numbers in array.java

--- Find EvnOdd numbers in array.java
+++ Find EvnOdd numbers in array.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 public class EvenOdd{
   public static void main(String [] args){
     int[] numbers= {1,2,3,4,5,6,7,8,9};
-    ArrayList evan=new ArrayList<>();
+    ArrayList even=new ArrayList<>();
     ArrayList odd=new ArrayList<>();
  
     for(int num: numbers){
@@ -16,3 +16,10 @@ public class EvenOdd{
     System.out.println("Odd Numbers: "+odd);
   }
 }  
+
+
+
+// Output:
+
+// Even Numbers: [2, 4, 6, 8]
+// Odd Numbers: [1, 3, 5, 7, 9]
