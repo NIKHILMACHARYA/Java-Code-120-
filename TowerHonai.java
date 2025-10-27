43be9a1a928034315661624c8b83a7246bf07328
Nikhil M Acharya <154912600+NIKHILMACHARYA@users.noreply.github.com>
2025-10-27 23:27:34 +0530

    Update TowerHonai.java

--- TowerHonai.java
+++ TowerHonai.java
@@ -1,18 +1,19 @@
 import java.util.*;
 
 public class TowerHonai{
-    public static void TowerofHonai(int n, string str, string helper, string dest ){
+    public static void TowerofHonai(int n, String src, String helper, String dest ){
         if(n==1){
             System.out.println("Transfer disk "+n+" from "+src+" to "+dest);
             return;
         }
         TowerofHonai(n-1, src, dest ,helper);
-        System.ou.println("Transfer disk "+n+" from "+src+" to "+dest);
+        System.out.println("Transfer disk "+n+" from "+src+" to "+dest);
         TowerofHonai(n-1, helper, src, dest);
     }
 
-    public static void main(string[] args){
+    public static void main(String[] args){
         int n=3;
-        TowerofHonai(n ,"S", "H", "D")
+        TowerofHonai(n ,"S", "H", "D");
+        
     }
 }    
